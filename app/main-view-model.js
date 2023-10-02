2832ca20221cc20ba6a4a7b9f1947de84b65f09d
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-10-02 23:56:35 +0530

    Format whitespace

--- app/main-view-model.js
+++ app/main-view-model.js
@@ -1,23 +1,22 @@
-import { Observable } from '@nativescript/core'
+import { Observable } from "@nativescript/core";
 
 function getMessage(counter) {
   if (counter <= 0) {
-    return 'Hoorraaay! You unlocked the NativeScript clicker achievement!'
+    return "Hoorraaay! You unlocked the NativeScript clicker achievement!";
   } else {
-    return `${counter} taps left`
+    return `${counter} taps left`;
   }
 }
 
 export function createViewModel() {
-  const viewModel = new Observable()
-  viewModel.counter = 42
-  viewModel.message = getMessage(viewModel.counter)
+  const viewModel = new Observable();
+  viewModel.counter = 42;
+  viewModel.message = getMessage(viewModel.counter);
 
   viewModel.onTap = () => {
-    viewModel.counter--
-    viewModel.set('message', getMessage(viewModel.counter))
-  }
+    viewModel.counter--;
+    viewModel.set("message", getMessage(viewModel.counter));
+  };
 
-  return viewModel
+  return viewModel;
 }
-
