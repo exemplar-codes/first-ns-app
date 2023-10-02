81584208af6fad5939b8cb5aa0f383eb15a617dc
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-10-03 00:12:47 +0530

    text box and changing view

--- app/main-view-model.js
+++ app/main-view-model.js
@@ -19,5 +19,12 @@ export function createViewModel() {
     viewModel.set("message", countMessage);
   };
 
+  // new code (my code)
+  viewModel.xHint = 2; // input placeholder
+  viewModel.xValue = "Hello, world"; // no need of setter
+  viewModel.xOnButtonClick = () => {
+    viewModel.set("message", viewModel.xValue);
+  };
+
   return viewModel;
 }
